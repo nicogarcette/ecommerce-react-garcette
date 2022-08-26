18baf6c796fb3f831dd41b1c4ee449da31b5350d
nicogarcette <nicolasgarcette@outlook.com>
2022-08-26 10:19:31 -0300

    componentes ItemList y Item creados

---
+++ src/components/Item.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Item=()=>{
+
+
+    return(
+        <div>
+
+        </div>
+    )
+
+
+
+}
+
+export default Item;
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItemList=()=>{
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default ItemList;
