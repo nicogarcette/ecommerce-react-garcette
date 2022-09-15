8870974bce3e5a64df36fd1944fcfef4e27ff291
nicogarcette <nicolasgarcette@outlook.com>
2022-09-15 20:38:36 -0300

    instalacion de firebase

--- src/utils/ThemeMui.js
+++ src/utils/ThemeMui.js
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 const theme = createTheme({
     palette:{
         primary:{
-            main:'#121858'
+            main:'#212121'
         }
     }
 
